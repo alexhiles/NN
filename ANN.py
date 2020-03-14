37e9b873b0269d7ef4c89f5803fc6877ac74b6df
alexhiles <alex.hiles@hotmail.com>
2020-03-14 00:05:51 +0000

    General ANN now implemented and working for arbitrary layers and neurons. naiveANN.py is specific to higham paper, ANN.py is not

--- ANN.py
+++ ANN.py
@@ -1,29 +1,44 @@
 from numpy import array, zeros, ones, arange, exp, dot, save, pi, linspace,\
                   matrix, ceil
-from numpy.random import randn, randint, uniform
+from numpy.random import randn, randint, uniform, normal
 from numpy.linalg import norm
 import matplotlib.pylab as plt
+import sys
 
 class GeneralNetwork:
     def __init__(self, number_of_layers, neurons_per_layer):
-        list_of_weight_matrices = [randn(neurons_per_layer[0], neurons_per_layer[0])]
-        list_of_bias_vectors    = [randn(neurons_per_layer[0], 1)]
 
-        self.weights   = list_of_weight_matrices
-        self.biases    = list_of_bias_vectors
+        try:
+            if number_of_layers != len(neurons_per_layer):
+                print("The length of the neuron vector has to match the number \
+                        of layers. Each entry refers to the number of neurons in\
+                        that layer!")
+                sys.exit()
+        except:
+            print("The variable number_of_layers must be an integer!")
+            print("The variable neurons_per_layer must be a list!")
+            sys.exit()
+
+        list_of_weight_matrices = [normal(size=(neurons_per_layer[0], \
+                                                neurons_per_layer[0]))]
+        list_of_bias_vectors    = [normal(size=(neurons_per_layer[0], 1))]
+
+        self.weights            = list_of_weight_matrices
+        self.biases             = list_of_bias_vectors
 
         self.number_of_layers   = number_of_layers
         self.neurons_per_layer  = neurons_per_layer
 
         for i in arange(1, len(neurons_per_layer)):
-            self.weights.append(randn(neurons_per_layer[i],\
-                                        neurons_per_layer[i - 1]))
-            self.biases.append(randn(neurons_per_layer[i], 1))
+            self.weights.append(normal(size=(neurons_per_layer[i],\
+                                        neurons_per_layer[i - 1])))
+            self.biases.append(normal(size=(neurons_per_layer[i], 1)))
 
     def activate(self, x, W, b):
+
         return 1 / (1 + exp(-(dot(W, x) + b)))
 
-    def train(self, Data, eta = 0.05, niter = 1000):
+    def train(self, Data, eta = 0.1, niter = 100000):
 
         cost   = zeros(niter)
         xtrain = zeros((Data.xtrain.shape[0], 1))
@@ -31,63 +46,80 @@ class GeneralNetwork:
         activation = []
         delta      = []
         for counter in arange(niter):
-            k = randint(Data.xtrain.shape[0])
-            xtrain[:, 0] = Data.xtrain[:, k]
+            k = randint(Data.xtrain.shape[1])
+            # Training Data
+            xtrain[:, 0], ytrain[:, 0] = Data.xtrain[:, k], Data.ytrain[:, k]
 
             # Forward Prop
             for s in arange(self.number_of_layers):
                 activation.append(self.activate(xtrain,            \
                         self.weights[s], self.biases[s]))
                 xtrain = activation[s]
-            ytrain[:, 0] = Data.ytrain[:, k]
-
             #  Back Prop
 
-            delta.append(activation[-1] * (1 - activation[-1]) * \
-                              activation[-1] - ytrain)
+        #    delta.append(activation[-1] * (1 - activation[-1]) * (activation[-1] - ytrain))
+        #    delta.append(activation[-2] * (1 - activation[-2]) * dot(self.weights[-1].T, delta[0]))
+        #    delta.append(activation[-3] * (1 - activation[-3]) * dot(self.weights[-2].T, delta[1]))
+
+
+            delta.append(activation[-1] * (1 - activation[-1]) * (activation[-1] - ytrain))
             for s in arange(0, self.number_of_layers-1):
                 delta.append(activation[-2 - s] * (1 - activation[-2 - s]) * \
                 dot(self.weights[-1  - s].T, delta[s]))
 
+
             #  Update
 
-            self.weights[0]  -= eta * delta[-1] * xtrain.T
-
-            for s in arange(0, self.number_of_layers):
-                self.biases[s]        -= eta * delta[s]
+            self.weights[0]  -= eta * delta[-1] * Data.xtrain[:, k].T
+            for s in arange(1, self.number_of_layers):
                 if s >= 1:
-                    self.weights[s]   -= eta * delta[-(s + 1)] * activation[s]
+                    self.weights[s]   -= eta * dot(delta[-(s + 1)], activation[s - 1].T)
 
+            deltaflip = delta
+            deltaflip.reverse()
+
+            for s in arange(self.number_of_layers):
+                self.biases[s] -= eta * deltaflip[s]
 
             activation = []
             delta      = []
 
-            #cost[counter] = self.cost_function(Data)
-            print(self.cost_function(Data))
+            cost[counter] = self.cost_function(Data)
+
         return cost
 
     def cost_function(self, Data):
         temp_cost = zeros((Data.xtrain.shape[1],1))
         x         = zeros((Data.xtrain.shape[0],1))
-        activation = []
+
         for i in arange(temp_cost.shape[0]):
             x[0,0], x[1,0] = Data.xtrain[0,i], Data.xtrain[1,i]
+
+    #        a2 = self.activate(x, self.weights[0],  self.biases[0])
+    #        a3 = self.activate(a2, self.weights[1], self.biases[1])
+    #        a4 = self.activate(a3, self.weights[2], self.biases[2])
+
             for s in arange(self.number_of_layers):
-                activation.append(self.activate(x,            \
-                        self.weights[s], self.biases[s]))
-                x = activation[s]
-            temp_cost[i] = norm(Data.ytrain - activation[-1], 2)
+                a = self.activate(x, self.weights[s], self.biases[s])
+                x = a
+            temp_cost[i] = norm(x.ravel() - Data.ytrain[:, i], 2)
         return norm(temp_cost, 2)**2
 
 class Data:
-    def __init__(self, number_of_data_points):
-        self.x           = linspace(0, 1, number_of_data_points)
+    def __init__(self, number_of_data_points,highamdata=True):
 
-        x1               = zeros((1, number_of_data_points))
-        x2               = zeros((1, number_of_data_points))
 
-        x1[0,:]          = uniform(0, -self.x + 1, self.x.shape[0])
-        x2[0,:]          = uniform(-self.x + 1, 1, self.x.shape[0])
+        if not highamdata:
+            self.x           = linspace(0, 1, number_of_data_points)
+            x1               = zeros((1, number_of_data_points))
+            x2               = zeros((1, number_of_data_points))
+            x1[0,:]          = uniform(0, -self.x + 1, self.x.shape[0])
+            x2[0,:]          = uniform(-self.x + 1, 1, self.x.shape[0])
+        else:
+
+            x1 = array([0.1, 0.3, 0.1, 0.6, 0.4, 0.6, 0.5, 0.9, 0.4, 0.7])
+            x2 = array([0.1, 0.4, 0.5, 0.9, 0.2, 0.3, 0.6, 0.2, 0.4, 0.6])
+
 
         y1               = zeros((1, int(number_of_data_points / 2)))
         y2               = ones((1,  int(number_of_data_points / 2)))
@@ -102,6 +134,9 @@ class Data:
         self.ytrain[1, int(number_of_data_points   / 2):] = y2
 
 if __name__ == '__main__':
-    Network = GeneralNetwork(3, [2, 3, 2])
-    Data    = Data(10)
-    Network.train(Data)
+    Network = GeneralNetwork(4, [2, 5, 5, 2])
+    Data    = Data(20)
+    cost = Network.train(Data)
+    import matplotlib.pylab as plt
+    plt.plot(cost)
+    plt.show()
---
+++ naiveANN.py
@@ -0,0 +1,69 @@
+import numpy as np
+from numpy.random import uniform, randint, randn, normal
+import sys
+def activate(x, W, b):
+    return 1 / (1 + np.exp(-(np.dot(W, x) + b)))
+
+def cost_function(W2,W3,W4,b2,b3,b4, x1, x2,y):
+
+    costvec = np.zeros((10, 1))
+    x       = np.zeros((2,  1))
+    for i in np.arange(costvec.shape[0]):
+        x[0,0], x[1,0] = x1[i], x2[i]
+        a2 = activate(x,  W2, b2)
+        a3 = activate(a2, W3, b3)
+        a4 = activate(a3, W4, b4)
+        costvec[i] = np.linalg.norm(a4.ravel()-y[:,i], 2)
+
+    return np.linalg.norm(costvec, 2)**2
+import matplotlib.pylab as plt
+
+x1 = np.array([0.1, 0.3, 0.1, 0.6, 0.4, 0.6, 0.5, 0.9, 0.4, 0.7])
+x2 = np.array([0.1, 0.4, 0.5, 0.9, 0.2, 0.3, 0.6, 0.2, 0.4, 0.6])
+
+y           = np.zeros((2, 10))
+y[0:1, 0:5] = np.ones((1,  5))
+y[0:1, 5: ] = np.zeros((1, 5))
+y[0:1, 5: ] = np.zeros((1, 5))
+y[1: , 5: ] = np.ones((1,  5))
+
+W2,W3,W4 = normal(size=(2,2)), normal(size=(3,2)), normal(size=(2, 3))
+b2,b3,b4 = normal(size=(2,1)), normal(size=(3,1)), normal(size=(2, 1))
+
+eta = 0.1
+Niter = 100000
+
+
+xvec = np.zeros((2,1))
+yvec = np.zeros((2,1))
+cost_value = np.zeros((Niter,1))
+for counter in np.arange(Niter):
+    k = randint(10)
+    xvec[0,0], xvec[1,0] = x1[k], x2[k]
+    yvec[:,0] = y[:, k]
+
+    # forward pass
+    a2 = activate(xvec, W2, b2)
+    a3 = activate(a2,   W3, b3)
+    a4 = activate(a3,   W4, b4)
+
+    # backward pass
+    delta4 = a4 * (1 - a4) * (a4 - yvec)
+
+    delta3 = a3 * (1 - a3) * np.dot(W4.T, delta4)
+
+    delta2 = a2 * (1 - a2) * np.dot(W3.T, delta3)
+
+    W2 -= eta * delta2 * xvec.T
+    W3 -= eta * delta3 * a2.T
+    W4 -= eta * delta4 * a3.T
+
+    b2 -= eta * delta2
+    b3 -= eta * delta3
+    b4 -= eta * delta4
+
+    cost_value[counter] = cost_function(W2,W3,W4,b2,b3,b4,x1, x2, y)
+import matplotlib.pylab as plt
+
+plt.plot(cost_value)
+plt.show()
